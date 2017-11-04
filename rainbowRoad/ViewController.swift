2bd270285344880cffcc271d7c3be3831caa3a28
Patrick Hayes <phayes2828@gmail.com>
2017-11-04 15:16:21 -0700

    complete

--- rainbowRoad/ViewController.swift
+++ rainbowRoad/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let colors: [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.rowHeight = 120
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -23,3 +30,14 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell", for: indexPath)
+        cell.backgroundColor = colors[indexPath.row]
+        return cell
+    }
+}
